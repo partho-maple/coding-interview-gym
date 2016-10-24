606a33a6ac7ecb6c1dc42dba78174eed2a8028b4
Partho Biswas <partho.maple@gmail.com>
2016-10-25 01:23:10 +0600

    Problem 3 solved.

---
+++ leet_code_problems/#3 - Longest Substring Without Repeating Characters.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//: Problem:    https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+extension Array where Element: Equatable {
+    
+    // Remove first collection element that is equal to the given `object`:
+    mutating func remove(object: Element) {
+        if let index = index(of: object) {
+            remove(at: index)
+        }
+    }
+}
+
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        let length = s.characters.count
+        var stringArray:[Character] = Array(s.characters) as [Character]
+        var traversedArray:[Character] = []
+        var i = 0, j = 0, ans = 0
+        
+        while i<length && j<length {
+            if !traversedArray.contains(stringArray[j]) {
+                traversedArray.append(stringArray[j])
+                j += 1
+                ans = max(ans, j-i)
+            } else {
+                traversedArray.remove(object: stringArray[i])
+                i+=1
+            }
+        }
+        
+        return ans
+    }
+}
