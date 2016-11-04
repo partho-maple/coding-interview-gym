a1a833c7ebf6ff86d78170d52e3742dbbd35ba1c
Partho Biswas <partho.maple@gmail.com>
2016-11-04 13:18:17 +0600

    One playground for all solutions.

---
+++ leet_code_problems/Leetcode_Solutions.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Testing code for each solution
+
+import UIKit
+
+
+// Solution test code for: #1 - Two Sum
+let inputArr = [0,4,3,0]
+let target = 0
+var solution_1 = Solution_1()
+solution_1.twoSum(inputArr, target)
+
