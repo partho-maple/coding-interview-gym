6a1d2e72a98f70175e5c5278a33b37ce5c6edf42
Partho Biswas <partho.biswas@aurea.com>
2017-01-30 23:25:46 +0600

    461. Hamming Distance >> Solved. >> https://leetcode.com/problems/hamming-distance/

--- leet_code_problems/Leetcode_Solutions.playground/Contents.swift
+++ leet_code_problems/Leetcode_Solutions.playground/Contents.swift
@@ -1,6 +1,8 @@
 //: Testing code for each solution
 
 import UIKit
+import Foundation
+
 
 
 //MARK: Solution test code for: #1 - Two Sum
@@ -9,3 +11,10 @@ let target = 0
 var solution_1 : Solution_1 = Solution_1()
 solution_1.twoSum(inputArr, target)
 
+
+
+let num = 22
+let str = String(num, radix: 2)
+print(str) // prints "10110"
+
+let charAtIndex = str[str.index(str.startIndex, offsetBy: 4)]
---
+++ leet_code_problems/Leetcode_Solutions.playground/Sources/#461 - Hamming Distance.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+//: Problem Link:  https://leetcode.com/problems/hamming-distance/
+
+class Solution_461 {
+    func hammingDistance(_ x: Int, _ y: Int) -> Int {
+        var hammingDistance: Int = 0
+        var maxLen = 0
+        
+        
+        var strX = String(x, radix: 2)
+        var strY = String(y, radix: 2)
+        if strX.characters.count > strY.characters.count {
+            maxLen = strX.characters.count
+            strY = self.pad(string: strY, toSize: maxLen)
+        } else {
+            maxLen = strY.characters.count
+            strX = self.pad(string: strX, toSize: maxLen)
+        }
+        
+        for index in 0..<maxLen {
+            let a = strX[strX.index(strX.startIndex, offsetBy: index)]
+            let b = strY[strY.index(strY.startIndex, offsetBy: index)]
+            
+            if a != b {
+                hammingDistance += 1
+            }
+        }
+        
+        
+        return hammingDistance;
+    }
+    
+    func pad(string : String, toSize: Int) -> String {
+        var padded = string
+        for _ in 0..<(toSize - string.characters.count) {
+            padded = "0" + padded
+        }
+        return padded
+    }
+}
