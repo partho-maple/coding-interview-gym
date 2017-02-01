4cb46f24b2d391fd6ffa641890e24510dc71f94c
Partho Biswas <partho.biswas@aurea.com>
2017-02-01 23:15:46 +0600

    #412 - Fizz Buzz

--- leet_code_problems/Leetcode_Solutions.playground/Contents.swift
+++ leet_code_problems/Leetcode_Solutions.playground/Contents.swift
@@ -5,39 +5,27 @@ import Foundation
 
 
 
-//MARK: Solution test code for: #1 - Two Sum
-let inputArr = [0,4,3,0]
-let target = 0
-var solution_1 : Solution_1 = Solution_1()
-solution_1.twoSum(inputArr, target)
+let n = 15
 
+var stringArr: [String] = []
 
-
-let num = 22
-let str = String(num, radix: 2)
-print(str) // prints "10110"
-
-let charAtIndex = str[str.index(str.startIndex, offsetBy: 4)]
-
-
-
-
-var strNum = String(5, radix: 2)
-var strComplement = ""
-
-for index in 0..<strNum.characters.count {
-    let charAtIndex = strNum[strNum.index(strNum.startIndex, offsetBy: index)]
-    print(charAtIndex)
-
-    if charAtIndex != "1" {
-        strComplement.append("1")
+for index in 1...n {
+    if ((index % 3) == 0) && ((index % 5) == 0) {
+        stringArr.append("FizzBuzz")
+        print("FizzBuzz")
+    } else if ((index % 3) == 0) {
+        stringArr.append("Fizz")
+        print("Fizz")
+    } else if ((index % 5) == 0) {
+        stringArr.append("Buzz")
+        print("Buzz")
     } else {
-        strComplement.append("0")
+        stringArr.append(String(index))
+        print(index)
     }
 }
 
 
-var complement = Int(String(strComplement), radix: 2)!
 
 
 
---
+++ leet_code_problems/Leetcode_Solutions.playground/Sources/#412 - Fizz Buzz.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+
+
+
+
+class Solution_412 {
+    func fizzBuzz(_ n: Int) -> [String] {
+        var stringArr: [String] = []
+        
+        for index in 1...n {
+            if ((index % 3) == 0) && ((index % 5) == 0) {
+                stringArr.append("FizzBuzz")
+                print("FizzBuzz")
+            } else if ((index % 3) == 0) {
+                stringArr.append("Fizz")
+                print("Fizz")
+            } else if ((index % 5) == 0) {
+                stringArr.append("Buzz")
+                print("Buzz")
+            } else {
+                stringArr.append(String(index))
+                print(index)
+            }
+        }
+        
+        return stringArr
+    }
+}
+
