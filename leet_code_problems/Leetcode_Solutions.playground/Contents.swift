59f057352208c023ba6b20ffed8730569ea96fdf
Partho Biswas <partho.biswas@aurea.com>
2017-02-01 22:24:21 +0600

    #476 - Number Complement

--- leet_code_problems/Leetcode_Solutions.playground/Contents.swift
+++ leet_code_problems/Leetcode_Solutions.playground/Contents.swift
@@ -18,3 +18,28 @@ let str = String(num, radix: 2)
 print(str) // prints "10110"
 
 let charAtIndex = str[str.index(str.startIndex, offsetBy: 4)]
+
+
+
+
+var strNum = String(5, radix: 2)
+var strComplement = ""
+
+for index in 0..<strNum.characters.count {
+    let charAtIndex = strNum[strNum.index(strNum.startIndex, offsetBy: index)]
+    print(charAtIndex)
+
+    if charAtIndex != "1" {
+        strComplement.append("1")
+    } else {
+        strComplement.append("0")
+    }
+}
+
+
+var complement = Int(String(strComplement), radix: 2)!
+
+
+
+
+
---
+++ leet_code_problems/Leetcode_Solutions.playground/Sources/#476 - Number Complement.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+
+
+
+class Solution_476 {
+    func findComplement(_ num: Int) -> Int {
+        
+        var strNum = String(num, radix: 2)
+        var strComplement = ""
+        
+        for index in 0..<strNum.characters.count {
+            let charAtIndex = strNum[strNum.index(strNum.startIndex, offsetBy: index)]
+            print(charAtIndex)
+            
+            if charAtIndex != "1" {
+                strComplement.append("1")
+            } else {
+                strComplement.append("0")
+            }
+        }
+        
+        
+        let complement = Int(String(strComplement), radix: 2)!
+        
+        return complement
+    }
+}
+
+
+
+
+
+
+
