83ce10b017fee7529328d047395c870ab768990b
Partho Biswas <partho.biswas@aurea.com>
2019-06-05 15:30:28 +0600

    BST Construction (Recursive and Iterative)

---
+++ Python/DS_Algo_Practice/src/algoexpert.io/BST_Construction_Iterative.py
@@ -0,0 +1,78 @@
+# https://www.algoexpert.io/questions/BST%20Construction
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+ # Average: O(log(n)) time | O(1) space
+ # Worst: O(n) time | O(1) space
+    def insert(self, value):
+        currentNode = self
+        while True:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+        return self
+
+    # Average: O(log(n)) time | O(1) space
+    # Worst: O(n) time | O(1) space
+    def contains(self, value):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                currentNode = currentNode.right
+            else:
+                return True
+        return False
+
+    # Average: O(log(n)) time | O(1) space
+    # Worst: O(n) time | O(1) space
+    def remove(self, value, parentNode=None):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.right
+            else:
+                if currentNode.left is not None and currentNode.right is not None:
+                    currentNode.value = currentNode.right.getMinValue()
+                    currentNode.right.remove(currentNode.value, currentNode)
+                elif parentNode is None:
+                    if currentNode.left is not None:
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.right
+                        currentNode.left = currentNode.left.left
+                    elif currentNode.right is not None:
+                        currentNode.value = currentNode.right.value
+                        currentNode.left = currentNode.right.left
+                        currentNode.right = currentNode.right.right
+                    else:
+                        currentNode.value = None
+                elif parentNode.left == currentNode:
+                    parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
+                elif parentNode.right == currentNode:
+                    parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right
+                break
+        return self
+
+    def getMinValue(self):
+        currentNode = self
+        while currentNode.left is not None:
+            currentNode = currentNode.left
+        return currentNode.value
---
+++ Python/DS_Algo_Practice/src/algoexpert.io/BST_Construction_Recursive.py
@@ -0,0 +1,69 @@
+# https://www.algoexpert.io/questions/BST%20Construction
+
+class BST:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
+        return self
+
+    def contains(self, value):
+        if value < self.value:
+            if self.value is None:
+                return False
+            else:
+                self.left.contains(value)
+        elif value > self.value:
+           if self.right is None:
+               return False
+           else:
+               self.right.contains(value)
+        else:
+            return True
+
+    def remove(self, value, parent = None):
+        if value < self.value:
+            if self.left is not None:
+                self.left.remove(value, self)
+        elif value > self.value:
+            if self.right is not None:
+                self.right.remove(value, self)
+        else:
+            if self.left is not None and self.right is not None:
+                self.value = self.right.get_min_value()
+                self.right.remove(self.value, self)
+            elif parent is None:
+                if self.left is not None:
+                    self.value = self.left.value
+                    self.right = self.left.right
+                    self.left = self.left.left
+                elif self.right is not None:
+                    self.value = self.right.value
+                    self.left = self.right.left
+                    self.right = self.right.right
+                else:
+                    self.value = None
+            elif parent.left == self:
+                parent.left = self.left if self.left is not None else self.right
+            elif parent.right == self:
+                parent.right = self.left if self.left is not None else self.right
+        return self
+
+    def get_min_value(self):
+        if self.left is None:
+            return self.value
+        else:
+            return self.left.get_min_value()
+
