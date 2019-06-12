d7a73f19239d02e6749486c1d062a1e8f7873b8b
Partho Biswas <partho.biswas@aurea.com>
2019-06-12 22:55:08 +0600

    alternative solution added for Levenshtein Distance or Edit Distance problem

--- Python/DS_Algo_Practice/src/algoexpert.io/Levenshtein_Distance.py
+++ Python/DS_Algo_Practice/src/algoexpert.io/Levenshtein_Distance.py
@@ -17,3 +17,29 @@ def levenshtein_distance(str1, str2):
     return edits[-1][-1] # this return the final value (last cell) of 2D array
 
 
+# Solution 2
+# O(nm) time | O (min(n, m)) space
+def levenshtein_distance(str1, str2):
+    small = str1 if len(str1) < len(str2) else str2 # this would be the column count
+    big = str1 if len(str1) >= len(str2) else str2  # this would be the row count
+    even_edits = [x for x in range(len(small) + 1)] # This essentially means the even rows
+    odd_edits = [None for x in range(len(small) + 1)]  # This essentially means the odd rows
+    for i in range(1, len(big) + 1):
+        if i % 2 == 1:
+            current_edits = odd_edits
+            previous_edits = even_edits
+        else:
+            current_edits = even_edits
+            previous_edits = odd_edits
+        current_edits[0] = i
+        for j in range(1, len(big) + 1):
+            if big[i - 1] == small[j - 1]
+                current_edits[j] = previous_edits[j - 1]
+            else:
+                current_edits[j] = 1 + min(previous_edits[j - 1], previous_edits[j], current_edits[j - 1])
+    return even_edits[-1] if len(big) % 2 == 0 else odd_edits[-1]
+
+
+
+
+
