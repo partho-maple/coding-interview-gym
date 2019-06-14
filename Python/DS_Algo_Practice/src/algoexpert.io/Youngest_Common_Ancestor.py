aaad48945fef8412ba0de72f3f4855bfce4f0da5
Partho Biswas <partho.biswas@aurea.com>
2019-06-15 00:26:10 +0600

    Lowest/youngest common ancestor

---
+++ Python/DS_Algo_Practice/src/algoexpert.io/Youngest_Common_Ancestor.py
@@ -0,0 +1,31 @@
+# https://www.algoexpert.io/questions/Youngest%20Common%20Ancestor
+
+
+# O(d) time - where d id the depth | O(1) space
+def get_youngest_common_ancestor(top_ancestor, descendant_one, descendant_two):
+    depth_one = get_descendant_depth(descendant_one, top_ancestor)
+    depth_two = get_descendant_depth(descendant_two, top_ancestor)
+    if depth_one > depth_two:
+        return backtrack_ancestral_tree()
+    else:
+        return backtrack_ancestral_tree()
+
+
+def get_descendant_depth(descendant, top_ancestor):
+    depth = 0
+    while descendant != top_ancestor:
+        descendant = descendant.ancestor
+        depth += 1
+    return depth
+
+
+def backtrack_ancestral_tree(lower_descendant, higher_descendant, diff):
+    while diff > 0:
+        lower_descendant = lower_descendant.ancestor
+        diff -= 1
+    while lower_descendant != higher_descendant:
+        lower_descendant = lower_descendant.ancestor
+        higher_descendant = higher_descendant.ancestor
+    return lower_descendant
+
+
