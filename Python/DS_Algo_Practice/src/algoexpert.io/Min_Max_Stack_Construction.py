b1a1c5fec038110d5fe6ede2fc3bb5691a656048
Partho Biswas <partho.biswas@aurea.com>
2019-06-19 10:10:14 +0600

    Min max stack construction

---
+++ Python/DS_Algo_Practice/src/algoexpert.io/Min_Max_Stack_Construction.py
@@ -0,0 +1,44 @@
+# https://www.algoexpert.io/questions/Min%20Max%20Stack%20Construction
+
+
+class MinMaxStack:
+
+    def __init__(self):
+        # only contains the min and max value at any given time
+        # min_max_stack is an array of dictionary, which is the same length of stack
+        self.min_max_stack = [] # an array of dictionary of min and max key
+        self.stack = []
+
+    # O(1) time | O(1) space
+    def peek(self):
+        return self.stack[len(self.stack) - 1]
+
+    # O(1) time | O(1) space
+    def pop(self):
+        self.min_max_stack.pop()
+        return self.stack.pop()
+
+    # O(1) time | O(1) space
+    def push(self, number):
+        new_min_max = {"min": number, "max": number}
+        if len(self.min_max_stack):
+            last_min_max = self.min_max_stack[len(self.min_max_stack) - 1]
+            new_min_max["min"] = min(last_min_max["min"], number)
+            new_min_max["max"] = min(last_min_max["max"], number)
+        self.min_max_stack.append(new_min_max)
+        self.stack.append(number)
+
+    # O(1) time | O(1) space
+    def get_min(self):
+        return self.min_max_stack[len(self.min_max_stack) - 1]["min"]
+
+    # O(1) time | O(1) space
+    def get_max(self):
+        return self.min_max_stack[len(self.min_max_stack) - 1]["max"]
+
+
+
+
+
+
+
