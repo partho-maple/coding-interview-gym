461abb5bbdbd8e88498705d634ff5c93f3490d46
Partho Biswas <partho.biswas@aurea.com>
2019-06-21 14:02:46 +0600

    Suffix Trie documentation updated

--- Python/DS_Algo_Practice/src/algoexpert.io/Suffix_Trie_Construction.py
+++ Python/DS_Algo_Practice/src/algoexpert.io/Suffix_Trie_Construction.py
@@ -1,5 +1,27 @@
 # https://www.algoexpert.io/questions/Suffix%20Trie%20Construction
 
+"""
+ The way we can build our Suffix-Trie is to use a bunch of hash tables or dictionaries. Where every node in the suffix
+ tree is gonna be a Key in a dictionary, pointing to another dictionary/HashTable (the value of the key will store of
+ the next node's dictionary directly so basically, it will be a nested dictionary of dictionary). And all the values in
+ the dictionary, will be the other nodes in the tree whose keys will be a specific letter that comes after the previous
+ letter, and that points to another hash table, and so on and so forth.
+
+Sample input (for creation):"babc"
+Sample output (for creation):
+{
+"c": {"*": True},
+"b": {
+        {"c": {"*": True},
+        {"a": {"b":
+                    {"c": {"*": True}}},
+     },
+"a": {
+        "b":
+             {"c": {"*": True}}},
+}
+"""
+
 
 class SuffixTrie:
 
