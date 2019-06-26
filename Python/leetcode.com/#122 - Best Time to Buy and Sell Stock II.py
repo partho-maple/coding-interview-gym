c84d10f03e50599bb610f222807982c7070276d2
Partho Biswas <partho.biswas@aurea.com>
2019-06-26 12:12:23 +0600

    leetcode 122. Best Time to Buy and Sell Stock II

---
+++ Python/leetcode.com/#122 - Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i + 1]:
+                profit = profit + (prices[i + 1] - prices[i])
+        return profit
+
+
+sol = Solution()
+prices = [1,2,3,4,5]
+profit = sol.maxProfit(prices)
+print("Profit: ", profit)
+
