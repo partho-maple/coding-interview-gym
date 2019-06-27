3f8374119665b63815b88480f2ba0eef32cd86e0
Partho Biswas <partho.biswas@aurea.com>
2019-06-27 22:12:06 +0600

    leetcode 88. Merge Sorted Array

---
+++ Python/leetcode.com/#88 - Merge Sorted Array.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        nums1_copy = nums1[:m]
+        nums1[:] = []
+
+        p1 = 0
+        p2 = 0
+
+        while p1 < m and p2 < n:
+            if nums1_copy[p1] < nums2[p2]:
+                nums1.append(nums1_copy[p1])
+                p1 += 1
+            else:
+                nums1.append(nums2[p2])
+                p2 += 1
+
+        if p1 < m:
+            nums1[p1 + p2:] = nums1_copy[p1:]
+        if p2 < n:
+            nums1[p1 + p2:] = nums2[p2:]
