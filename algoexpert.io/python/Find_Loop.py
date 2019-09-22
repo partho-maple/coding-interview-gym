6dc6257e28ddb2779b38c0349138a8d4152ccc3f
Partho Biswas <partho.biswas@aurea.com>
2019-09-22 10:37:22 +0600

    find loop in. linked list

---
+++ algoexpert.io/python/Find_Loop.py
@@ -0,0 +1,13 @@
+
+# O(n) time | O(1) space
+def findLoop(head):
+    first = head.next
+    second = head.next.next
+    while first != second:
+        first = first.next
+        second = second.next.next
+    first = head
+    while first != second:
+        first = first.next
+        second = second.next
+    return first
