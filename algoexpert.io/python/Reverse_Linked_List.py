abfc19c28593ea77fc8742f20f7b3a24979e8ee5
Partho Biswas <partho.biswas@aurea.com>
2019-09-22 11:20:45 +0600

    Reverse_Linked_List

---
+++ algoexpert.io/python/Reverse_Linked_List.py
@@ -0,0 +1,10 @@
+
+# O(n) time | O(1) space
+def reverseLinkedList(head):
+    p1, p2 = None, head
+    while p2 is not None:
+        p3 = p2.next
+        p2.next = p1
+        p1 = p2
+        p2 = p3
+    return p1
