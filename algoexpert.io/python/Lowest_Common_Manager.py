effcfde31bb50ec556971d62a401b68524c0da14
Partho Biswas <partho.biswas@aurea.com>
2019-09-23 11:19:52 +0600

    lowest common manager

---
+++ algoexpert.io/python/Lowest_Common_Manager.py
@@ -0,0 +1,32 @@
+
+
+class OrgInfo:
+    def __init__(self, lowestCommonManager, numImportantReports):
+        self.lowestCommonManager = lowestCommonManager
+        self.numImportantReports = numImportantReports
+
+class Node:
+    def __init__(self, directReports):
+        self.directReports = directReports
+
+
+# O(n) time | O(d) spaces
+def getLowestCommonnManager(topManager: Node, reportOne, reportTwo):
+    return getOrgInfo(topManager, reportOne, reportTwo).lowestCommonManager
+
+
+def getOrgInfo(manager: Node, reportOne, reportTwo):
+    numImportantReports = 0
+    for directReport in manager.directReports:
+        orgInfo = getOrgInfo(directReport, reportOne, reportTwo)
+        if orgInfo.lowestCommonManager is not None:
+            return orgInfo
+        numImportantReports += orgInfo.numImportantReports
+    if manager == reportOne or manager == reportTwo:
+        numImportantReports += 1
+    lowestCommonManager = manager if numImportantReports == 2 else None
+    return OrgInfo(lowestCommonManager, numImportantReports)
+
+
+
+
