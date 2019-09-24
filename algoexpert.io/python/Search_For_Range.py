7d8e120cbf44ebdf828c8b1931f8dd02e290c69d
Partho Biswas <partho.biswas@aurea.com>
2019-09-24 10:45:27 +0600

    search for range

---
+++ algoexpert.io/python/Search_For_Range.py
@@ -0,0 +1,68 @@
+
+
+
+# Solution #1   -   Recursive SOlution
+# O(logn) time  |  O(logn) space
+def searchForRange(array,  target):
+    finalRange = [-1, -1]
+    alteredBinarySearch(array, target, 0, len(array) - 1, finalRange, True)
+    alteredBinarySearch(array, target, 0, len(array) - 1, finalRange, False)
+    return finalRange
+
+
+def alteredBinarySearch(array, target, left, right, finalRange, goLeft):
+    if left > right:
+        return
+    mid = (left + right) // 2
+    if array[mid] < target:
+        alteredBinarySearch(array, target, mid + 1, right, finalRange, goLeft)
+    elif array[mid] > target:
+        alteredBinarySearch(array, target, left, mid - 1, finalRange, goLeft)
+    else:
+        if goLeft:
+            if mid == 0 or array[mid - 1] != target:
+                finalRange[0] = mid
+            else:
+                alteredBinarySearch(array, target, left, mid - 1, finalRange, goLeft)
+        else:
+            if mid == len(array) - 1 or array[mid + 1] != target:
+                finalRange[1] = mid
+            else:
+                alteredBinarySearch(array, target, mid + 1, right, finalRange, goLeft)
+
+
+
+
+
+# Solution #2   -   Iterative SOlution
+# O(logn) time  |  O(1) space
+def searchForRange(array,  target):
+    finalRange = [-1, -1]
+    alteredBinarySearch(array, target, 0, len(array) - 1, finalRange, True)
+    alteredBinarySearch(array, target, 0, len(array) - 1, finalRange, False)
+    return finalRange
+
+
+def alteredBinarySearch(array, target, left, right, finalRange, goLeft):
+    while left <= right:
+        mid = (left + right) // 2
+        if array[mid] < target:
+            left = mid + 1
+        elif array[mid] > target:
+            right = mid - 1
+        else:
+            if goLeft:
+                if mid == 0 or array[mid - 1] != target:
+                    finalRange[0] = mid
+                else:
+                    right = mid - 1
+            else:
+                if mid == len(array) - 1 or array[mid + 1] != target:
+                    finalRange[1] = mid
+                else:
+                    left = mid + 1
+
+
+
+
+
