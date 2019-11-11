1005ffd3bd93acafc718e51b32a9a3569a9f6299
Partho Biswas <partho.biswas@aurea.com>
2019-11-11 12:05:34 +0600

    solution updated

--- leetcode.com/python/122_Best_Time_to_Buy_and_Sell_Stock_II.py
+++ leetcode.com/python/122_Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -7,7 +7,7 @@ class Solution(object):
         profit = 0
         for i in range(len(prices) - 1):
             if prices[i] < prices[i + 1]:
-                profit = profit + (prices[i + 1] - prices[i])
+                profit += (prices[i + 1] - prices[i])
         return profit
 
 
