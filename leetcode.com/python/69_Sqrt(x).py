b883c8d083f57cfbb6c5f96e525791d89999b206
Partho Biswas <partho.biswas@aurea.com>
2019-11-21 01:35:22 +0600

    69. Sqrt(x)

---
+++ leetcode.com/python/69_Sqrt(x).py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 2:
+            return x
+        left, right = 0, x // 2
+        while left <= right:
+            mid = left + (right - left) // 2
+            num = mid * mid
+            if num > x:
+                right = mid - 1
+            elif num < x:
+                left = mid + 1
+            else:
+                return mid
+        return right
+
+
+
+
+sol = Solution()
+x = 6
+out = sol.mySqrt(x)
+print("Res: ", out)
