ee3196b64dbb2af8e03deae4cb897c989dba24b4
Partho Biswas <partho.biswas@aurea.com>
2019-11-21 22:08:13 +0600

    33. Search in Rotated Sorted Array

---
+++ leetcode.com/python/33_Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,60 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return -1
+        if n == 1:
+            return 0 if nums[0] == target else -1
+        rotatingIdx = self.findRotatingIndex(nums, 0, n - 1)
+        if nums[rotatingIdx] == target:
+            return rotatingIdx
+        if rotatingIdx == 0:
+            return self.binarySearch(nums, target, 0, n - 1)
+        if target >= nums[0]:
+            return self.binarySearch(nums, target, 0, rotatingIdx - 1)
+        else:
+            return self.binarySearch(nums, target, rotatingIdx, n - 1)
+
+    def findRotatingIndex(self, nums, left, right):
+        if nums[left] < nums[right]:  # nums is not rotated, it's already sorted
+            return 0
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[mid + 1]:  # we have found our rotating index
+                return mid + 1
+            elif (nums[mid] < nums[mid + 1]) and (nums[mid] < nums[mid - 1]):  # we have found our rotating index
+                return mid
+            else:
+                if nums[mid] > nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+    def binarySearch(self, nums, target, left, right):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
+
+
+sol = Solution()
+nums = [4,5,1,2,3]
+target = 1
+out = sol.search(nums, target)
+print("Res: ", out)
