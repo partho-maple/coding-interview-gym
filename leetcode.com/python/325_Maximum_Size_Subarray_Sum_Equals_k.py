f3dd70f0d7abd1589f13423c772af0c0e0816a76
Partho Biswas <partho.biswas@aurea.com>
2019-12-06 21:37:13 +0600

    325. Maximum Size Subarray Sum Equals k

---
+++ leetcode.com/python/325_Maximum_Size_Subarray_Sum_Equals_k.py
@@ -0,0 +1,62 @@
+from collections import Counter
+
+class Solution(object):
+    def maxSubArrayLen(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        # Sliding Window -- No, because the array contains negative number
+        # Dictionary + prefixSum technique
+        maxSubarrayLen, currentSum, prefixSum = 0, 0, 0
+        prefixSumIndexCounter = Counter()
+        prefixSumIndexCounter[0] = -1
+        for idx in range(len(nums)):
+            currentSum += nums[idx] #  increment current sum
+            prefixSum = currentSum - k
+            if currentSum == k:
+                maxSubarrayLen = idx + 1
+            elif prefixSum in prefixSumIndexCounter: # check if there is a prefix subarray we can take out to reach k
+                currentSubarrayLen = idx - prefixSumIndexCounter[prefixSum]
+                maxSubarrayLen = max(maxSubarrayLen, currentSubarrayLen)
+            if currentSum not in prefixSumIndexCounter:
+                prefixSumIndexCounter[currentSum] = idx
+        return maxSubarrayLen
+
+
+
+class Solution(object):
+    def maxSubArrayLen(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        # Sliding Window -- No, because the array contains negative number
+        # Dictionary + prefixSum technique
+        maxSubarrayLen, currentSum, prefixSum = 0, 0, 0
+        prefixSumIndexCounter = Counter()
+        prefixSumIndexCounter[0] = -1
+        for idx in range(len(nums)):
+            currentSum += nums[idx] #  increment current sum
+            prefixSum = currentSum - k
+            if currentSum == k:
+                maxSubarrayLen = idx + 1
+            elif prefixSum in prefixSumIndexCounter: # check if there is a prefix subarray we can take out to reach k
+                currentSubarrayLen = idx - prefixSumIndexCounter[prefixSum]
+                maxSubarrayLen = max(maxSubarrayLen, currentSubarrayLen)
+            if currentSum not in prefixSumIndexCounter:
+                prefixSumIndexCounter[currentSum] = idx
+        return maxSubarrayLen
+
+
+sol = Solution()
+# nums = [1, -1, 5, -2, 3]
+# k = 3
+# nums = [-2,-1,2,1]
+# k = 1
+nums = [1,0,-1]
+k = -1
+out = sol.maxSubArrayLen(nums, k)
+print("Res: ", out)
