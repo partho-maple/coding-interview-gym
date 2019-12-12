1265b76c44b9608e3f6fd6a6bcfbb9ccc4f173c1
Partho Biswas <partho.biswas@aurea.com>
2019-12-12 23:50:45 +0600

    268. Missing Number

--- leetcode.com/python/268_Missing_Number.py
+++ leetcode.com/python/268_Missing_Number.py
@@ -1,3 +1,5 @@
+
+# Bit-Manipulation approach
 class Solution(object):
     def missingNumber(self, nums):
         """
@@ -8,4 +10,27 @@ class Solution(object):
         for i, num in enumerate(nums):
             missing ^= i ^ num
         return missing
+
+
+
+
+
+# Cyclic-Sort approach
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        currentIndex, length = 0, len(nums)
+        while currentIndex < length:
+            finalIndex = nums[currentIndex]
+            if nums[currentIndex] < length and nums[currentIndex] != nums[finalIndex]:
+                nums[currentIndex], nums[finalIndex] = nums[finalIndex], nums[currentIndex]
+            else:
+                currentIndex += 1
+        for index in range(length):     # find the first number missing from its index, that will be our required number
+            if nums[index] != index:
+                return index
+        return length
 
