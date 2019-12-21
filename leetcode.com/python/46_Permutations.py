5f7f583a35bfd1b91be6852e032ae5059f2dacc3
Partho Biswas <partho.biswas@aurea.com>
2019-12-22 03:05:42 +0600

    46 modified

--- leetcode.com/python/46_Permutations.py
+++ leetcode.com/python/46_Permutations.py
@@ -1,38 +1,38 @@
+# S0urce:   https://tinyurl.com/ujv54ef
 class Solution(object):
     def permute(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        allPermutations = []
-        runningChoices = []
-        self.generatePermutation(runningChoices, nums, allPermutations)
+        allPermutations, runningPermutation = [], []
+        self.generatePermutation(runningPermutation, nums, allPermutations)
         return allPermutations
 
-    def generatePermutation(self, runningChoices, originalNums, allPermutations):
-        if len(runningChoices) == len(originalNums):
-            finalPermutation = [choice for choice in runningChoices]
+    def generatePermutation(self, runningPermutation, originalNums, allPermutations):
+        if len(runningPermutation) == len(originalNums):
+            finalPermutation = list(runningPermutation)
             allPermutations.append(finalPermutation)
             return
 
         # Every stack frame of this function call represents the expression of trying (almost) all items in every "slot" in the array.
         # The recursion stops when we are choosing on 1 past the final "slot".
         for i in range(len(originalNums)):
-            choice = originalNums[i]
+            num = originalNums[i]
 
             # Skip if element already exists in 'runningChoices'
-            if choice in runningChoices:
+            if num in runningPermutation:
                 continue
 
             # 1.) Choose - Add the item to the 'runningChoices'
-            runningChoices.append(choice)
+            runningPermutation.append(num)
 
             # 2.) Explore - Recurse on the choice
-            self.generatePermutation(runningChoices, originalNums, allPermutations)
+            self.generatePermutation(runningPermutation, originalNums, allPermutations)
 
             # 3.) Unchoose - We have returned from the recursion, remove the choice we made.
             # The next iteration will try another item in the "slot" we are working on.
-            runningChoices.pop()
+            runningPermutation.pop()
 
 
 
