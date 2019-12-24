8d2aa1a2669df060f255c1f3397247280c18d87a
Partho Biswas <partho.biswas@aurea.com>
2019-12-24 13:12:11 +0600

    code updated

--- leetcode.com/python/95_Unique_Binary_Search_Trees_II.py
+++ leetcode.com/python/95_Unique_Binary_Search_Trees_II.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution(object):
     def generateTrees(self, n):
@@ -27,11 +27,11 @@ class Solution(object):
             right_trees = self.generate_trees(i + 1, end)
 
             # connect left and right subtree to root i
-            for l in left_trees:
-                for r in right_trees:
+            for left_tree in left_trees:
+                for right_tree in right_trees:
                     current_tree = TreeNode(i)
-                    current_tree.left = l
-                    current_tree.right = r
+                    current_tree.left = left_tree
+                    current_tree.right = right_tree
                     all_trees.append(current_tree)
 
         return all_trees
--- leetcode.com/python/96_Unique_Binary_Search_Trees.py
+++ leetcode.com/python/96_Unique_Binary_Search_Trees.py
@@ -1,3 +1,4 @@
+# Source:   https://tinyurl.com/ut8wh4u   and    https://tinyurl.com/wuzpywc    and     https://tinyurl.com/vb3s4jm
 class Solution(object):
     def numTrees(self, n):
         """
@@ -10,3 +11,58 @@ class Solution(object):
             for j in range(1, i + 1):
                 G[i] += G[j - 1] * G[i - j]
         return G[n]
+
+
+
+
+
+# Source:   https://tinyurl.com/yx2ktbup
+# Getting TLE
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 1:
+            return 1
+        totalBstCount = 0
+        for nodeIdx in range(1, n + 1):
+            # making 'i' root of the tree
+            bstCountOfLeftSubTree = self.numTrees(nodeIdx - 1)
+            bstCountOfRightSubTree = self.numTrees(n - nodeIdx)
+            totalBstCount += (bstCountOfLeftSubTree * bstCountOfRightSubTree)
+        return totalBstCount
+
+
+
+
+from collections import defaultdict
+# Source:   https://tinyurl.com/yx2ktbup
+# Memoized version of the above solution
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        resultCache = defaultdict()
+        return self.numTreesHelper(n, resultCache)
+
+    def numTreesHelper(self, n, resultCache):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n in resultCache:
+            return resultCache[n]
+        if n <= 1:
+            return 1
+        totalBstCount = 0
+        for nodeIdx in range(1, n + 1):
+            # making 'i' root of the tree
+            bstCountOfLeftSubTree = self.numTreesHelper(nodeIdx - 1, resultCache)
+            bstCountOfRightSubTree = self.numTreesHelper(n - nodeIdx, resultCache)
+            totalBstCount += (bstCountOfLeftSubTree * bstCountOfRightSubTree)
+        resultCache[n] = totalBstCount
+        return totalBstCount
