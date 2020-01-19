1359d1feb42501d602c41a57525db49c5b975fbd
Partho Biswas <partho.biswas@aurea.com>
2020-01-19 17:23:19 +0600

    1066. Campus Bikes II

--- leetcode.com/python/1066_Campus_Bikes_II.py
+++ leetcode.com/python/1066_Campus_Bikes_II.py
@@ -1,40 +1,90 @@
+# Backtracking with memoization - tle
 class Solution(object):
-
-    # Backtrackng approach. Getting TLE. fix it.
     def assignBikes(self, workers, bikes):
         """
         :type workers: List[List[int]]
         :type bikes: List[List[int]]
         :rtype: int
         """
-        allDistances = []
-        self.calculateDistances(workers, bikes, allDistances, 0)
-        print(allDistances)
-        return min(allDistances)
+        minDistances = float("inf")
+        usedBikes = [False] * (len(bikes))
+        minDistances = self.calculateDistancesDFS(workers, 0, bikes, usedBikes, minDistances, 0)
+        return minDistances
 
-
-    def calculateDistances(self, workers, bikes, allDistances, runningDistance):
-        if len(workers) == 0:
-            allDistances.append(runningDistance)
-            return
-        for i in range(len(workers)):
-            for j in range(len(bikes)):
-                manhuttonDistance = self.getManhattanDistance(workers[i], bikes[j])
+    def calculateDistancesDFS(self, workers, workerIdx, bikes, usedBikes, minDistances, runningDistance):
+        if len(workers) == workerIdx:
+            minDistances = min(minDistances, runningDistance)
+            return minDistances
+        if runningDistance >= minDistances:
+            return minDistances
+        for i in range(len(bikes)):
+            if not usedBikes[i]:
+                manhuttonDistance = self.getManhattanDistance(workers[workerIdx], bikes[i])
                 runningDistance += manhuttonDistance
-                worker = workers[i]
-                bike = bikes[j]
-                workers.pop(i)
-                bikes.pop(j)
-                self.calculateDistances(workers, bikes, allDistances, runningDistance)
+                usedBikes[i] = True
+                minDistances = self.calculateDistancesDFS(workers, workerIdx + 1, bikes, usedBikes, minDistances, runningDistance)
                 runningDistance -= manhuttonDistance
-                workers.insert(i, worker)
-                bikes.insert(j, bike)
-
+                usedBikes[i] = False
+        return minDistances
 
     def getManhattanDistance(self, worker, bike):
         return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1])
 
 
+
+
+
+
+
+
+
+
+
+import heapq
+from collections import defaultdict
+
+# Source:  https://tinyurl.com/r4wtfbo   Accepted
+# Augmented graph:
+# vertex: (worker i, chosen bikes).
+# edge: mahatan distance.
+# Apply Dijkstra.
+
+class Solution:
+    def assignBikes(self, workers, bikes):
+        """
+        :type workers: List[List[int]]
+        :type bikes: List[List[int]]
+        :rtype: int
+        """
+
+        # each node int priority queue (cost, number of assigned workers, bikes status)
+        pq = [(0, 0, '0' * len(bikes))]
+
+        # optimal cost amongst all paths to reach a node
+        optimal = defaultdict(lambda: float('inf'))
+
+        while pq:
+            cost, i, bike_status = heapq.heappop(pq)
+
+            # early stopping with termination condition
+            if i == len(workers):
+                return cost
+
+                # generate successors. The next worker to be assigned is at index i
+            for j, b in enumerate(bikes):
+                if bike_status[j] != '1':
+                    new_cost = cost + self.manhattan(workers[i], b)
+                    new_bike_status = bike_status[:j] + '1' + bike_status[j + 1:]
+
+                    # update optimal cost if a new successor appears with lower cost to the same node
+                    if new_cost < optimal[(i + 1, new_bike_status)]:
+                        optimal[(i + 1, new_bike_status)] = new_cost
+                        heapq.heappush(pq, (new_cost, i + 1, new_bike_status))
+
+        return -1
+
+    def manhattan(self, a, b):
+        return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
 sol = Solution()
