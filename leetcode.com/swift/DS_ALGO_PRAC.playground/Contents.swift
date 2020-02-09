57a6e5944728dc60aa1667efad58b074097a1c94
Partho Biswas <partho.biswas@aurea.com>
2020-02-09 18:05:10 +0600

    no message

---
+++ leetcode.com/swift/DS_ALGO_PRAC.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+import Foundation
+
+class Solution {
+    func maxProduct(_ nums: [Int]) -> Int {
+        var maxProd = 1
+        var minProd = 1
+        var best = nums[0]
+    }
+}
+
