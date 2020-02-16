59df4f49897a097b15ede155e493b4e522a37732
Partho Biswas <partho.biswas@aurea.com>
2020-02-17 00:42:57 +0600

    173. Binary Search Tree Iterator

---
+++ leetcode.com/python/173_Binary_Search_Tree_Iterator.py
@@ -0,0 +1,76 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Approach 1: https://tinyurl.com/smu9ku3
+class BSTIterator(object):
+
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.nodesSorted = []
+        self.nextIndex = 0
+        self.__inorder(root)
+
+    def __inorder(self, root):
+        if not root:
+            return
+        self.__inorder(root.left)
+        self.nodesSorted.append(root.val)
+        self.__inorder(root.right)
+
+    def next(self):
+        """
+        @return the next smallest number
+        :rtype: int
+        """
+        value = self.nodesSorted[self.nextIndex]
+        self.nextIndex += 1
+        return value
+
+    def hasNext(self):
+        """
+        @return whether we have a next smallest number
+        :rtype: bool
+        """
+        return self.nextIndex < len(self.nodesSorted)
+
+
+
+
+
+# Approach 2: https://tinyurl.com/smu9ku3
+class BSTIterator(object):
+
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.stack = []
+        self.__leftMostInorder(root)
+
+    def __leftMostInorder(self, root):
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+    def next(self):
+        """
+        @return the next smallest number
+        :rtype: int
+        """
+        topMostNode = self.stack.pop()
+        if topMostNode.right:
+            self.__leftMostInorder(topMostNode.right)
+        return topMostNode.val
+
+    def hasNext(self):
+        """
+        @return whether we have a next smallest number
+        :rtype: bool
+        """
+        return len(self.stack) > 0
