b4829768e792ff03cb3dd8bd884cbda77ad23e4c
Partho Biswas <partho.biswas@aurea.com>
2020-04-04 21:33:19 +0600

    no message

--- leetcode.com/python/162_Find_Peak_Element.py
+++ leetcode.com/python/162_Find_Peak_Element.py
@@ -1,5 +1,4 @@
-
-# # Recursive approach
+# Recursive Approach
 # class Solution(object):
 #     def findPeakElement(self, nums):
 #         """
@@ -7,7 +6,7 @@
 #         :rtype: int
 #         """
 #         return self.findPeakElementHelper(nums, 0, len(nums) - 1)
-#
+
 #     def findPeakElementHelper(self, nums, left, right):
 #         if left == right:
 #             return left
@@ -17,7 +16,8 @@
 #         else:
 #             return self.findPeakElementHelper(nums, mid + 1, right)
 
-# Iterative solution
+
+# Time: O(log n) uses Binary search
 class Solution(object):
     def findPeakElement(self, nums):
         """
@@ -28,8 +28,24 @@ class Solution(object):
         while left < right:
             mid = (left + right) // 2
             if nums[mid] > nums[mid + 1]:
-                right =  mid
+                right = mid
             else:
                 left = mid + 1
         return left
 
+
+# Solution. during Mock test
+# Time: O(n) uses Linear search
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        peak = 0
+        if len(nums) <= 0:
+            return peak
+        for i in range(1, len(nums)):
+            if nums[i] > nums[peak]:
+                peak = i
+        return peak
--- leetcode.com/python/459_Repeated_Substring_Pattern.py
+++ leetcode.com/python/459_Repeated_Substring_Pattern.py
@@ -9,3 +9,26 @@ class Solution(object):
         ss = s + s
         ss = ss[1:-1]
         return ss.find(s) != -1
+
+
+
+# My solution duting Mock contest
+from collections import Counter
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s) < 0:
+            return False
+        L = len(s)
+        for i in range(L // 2):
+            if L % (i + 1) == 0:
+                pieces = []
+                for j in range(0, L, i + 1):
+                    pieces.append(s[j:j + i + 1])
+                counter = Counter(pieces)
+                if len(counter) == 1:
+                    return True
+        return False
