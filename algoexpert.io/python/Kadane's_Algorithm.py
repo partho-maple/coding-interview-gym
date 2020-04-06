b9edce1b6122a85545ef5245a6d8c198c0375a0a
Partho Biswas <partho.biswas@aurea.com>
2020-04-06 20:17:07 +0600

    code updated

--- algoexpert.io/python/Kadane's_Algorithm.py
+++ algoexpert.io/python/Kadane's_Algorithm.py
@@ -2,12 +2,12 @@
 
 
 # O(n) time | O(1) space - where n is the length of the   input array
-def kadanes_algorithm(array):
-    max_ending_here = array[0]
-    max_so_far = array[0]
-    for i in range(1, len(array)):
-        num = array[i]
-        max_ending_here = max(num, max_ending_here + num)
-        max_so_far = max(max_so_far, max_ending_here)
-    return max_so_far
+def kadanesAlgorithm(array):
+    maxEdingHere, maxSoFar = array[0], array[0]
+	for i in range(1, len(array)):
+		num = array[i]
+		maxEdingAtPrevIdx = maxEdingHere
+		maxEdingHere = max(num, num + maxEdingAtPrevIdx)
+		maxSoFar = max(maxSoFar, maxEdingHere)
+	return maxSoFar
 
--- algoexpert.io/python/Selection_Sort.py
+++ algoexpert.io/python/Selection_Sort.py
@@ -4,17 +4,16 @@
 # Best: O(n^2) time | O(1) space
 # Avarage: O(n^2) time | O(1) space
 # Worst: O(n^2) time | O(1) space
-def selection_sort(input_array):
+def selectionSort(array):
     current_index = 0
-    while current_index < len(input_array) - 1:
+    while current_index < len(array) - 1:
         smallest_index = current_index
-        for i in range(current_index + 1, len(input_array)):
-            if input_array[i] < input_array[current_index]:
+        for i in range(current_index + 1, len(array)):
+            if array[i] < array[smallest_index]:
                 smallest_index = i
-            swap(current_index, smallest_index, input_array)
-            current_index += 1
-    return input_array
-
+        swap(current_index, smallest_index, array)
+        current_index += 1
+    return array
 
 def swap(i, j, array):
     array[i], array[j] = array[j], array[i]
