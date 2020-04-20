de2ce65a62555938d0c61b6029e029bf03399b7d
Partho Biswas <partho.biswas@aurea.com>
2020-04-20 20:28:08 +0600

    no message

--- algoexpert.io/python/Smallest_Difference.py
+++ algoexpert.io/python/Smallest_Difference.py
@@ -1,29 +1,20 @@
-# https://www.algoexpert.io/questions/Smallest%20Difference
-
 # O(nlog(n) + mlon(m)) time | O(1) space
-def smallest_difference(array_one, array_two):
-    array_one.sort()
-    array_two.sort()
-    result_pair = []
-    index_one = 0
-    index_two = 0
-    smallest = float("inf")
-    current = float("inf")
+def smallestDifference(arrayOne, arrayTwo):
+	array_one, array_two = sorted(arrayOne), sorted(arrayTwo)
+    index_one, index_two = 0, 0
+    smallestDiff = float("inf")
+	result_pair = []
     while index_one < len(array_one) and index_two < len(array_two):
         first_num = array_one[index_one]
         second_num = array_two[index_two]
-        if first_num < second_num:
-            current = second_num - first_num
-            index_one += 1
-        elif second_num < first_num:
-            current = first_num - second_num
-            index_two += 1
-        else:
-            return [first_num, second_num]
-
-        if smallest > current:
-            smallest = current
-            result_pair = [first_num, second_num]
+		currentDiff = abs(first_num - second_num)
+        if currentDiff < smallestDiff:
+			smallestDiff = currentDiff
+			result_pair = [first_num, second_num]
+		if first_num <= second_num:
+			index_one += 1
+		else:
+			index_two += 1
     return result_pair
     
 
