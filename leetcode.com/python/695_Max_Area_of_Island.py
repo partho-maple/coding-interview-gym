3256abcb84ae12f4a975d2b7eefad110d1cdc85f
Partho Biswas <partho.biswas@aurea.com>
2020-04-26 22:34:39 +0600

    no message

--- leetcode.com/python/695_Max_Area_of_Island.py
+++ leetcode.com/python/695_Max_Area_of_Island.py
@@ -56,6 +56,58 @@ class Solution(object):
 
 
 
+
+
+
+
+
+# My Own solution during MOCK
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        sizes = [0]
+        visited = [[False for _ in row] for row in grid]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if not visited[i][j] and grid[i][j] == 1:
+                    self.traverseNode(i, j, grid, visited, sizes)
+        sizes.sort()
+        return sizes[-1]
+
+    def traverseNode(self, row, col, grid, visited, sizes):
+        nodesStack = [[row, col]]
+        currentSize = 0
+        while nodesStack:
+            i, j = nodesStack.pop()
+            if visited[i][j] == False:
+                visited[i][j] = True
+                currentSize += 1
+                unvisitedValidNeighbours = self.getUnvisitedValidNeighbours(i, j, grid, visited)
+                nodesStack.extend(unvisitedValidNeighbours)
+        sizes.append(currentSize)
+
+    def getUnvisitedValidNeighbours(self, row, col, grid, visited):
+        neighbours = []
+        if row > 0 and grid[row - 1][col] == 1 and not visited[row - 1][col]:
+            neighbours.append([row - 1, col])
+        if row < len(grid) - 1 and grid[row + 1][col] and not visited[row + 1][col]:
+            neighbours.append([row + 1, col])
+        if col > 0 and grid[row][col - 1] and not visited[row][col - 1]:
+            neighbours.append([row, col - 1])
+        if col < len(grid[0]) - 1 and grid[row][col + 1] and not visited[row][col + 1]:
+            neighbours.append([row, col + 1])
+        return neighbours
+
+
+
+
+
+
+
+
 # Driver code
 sol = Solution()
 # grid = [[1,1,0,0,0],
