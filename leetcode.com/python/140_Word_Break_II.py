ed83c52f960f14e88c0da5e41025ad9db5151f5e
Partho Biswas <partho.biswas@aurea.com>
2020-05-08 18:36:38 +0600

    no message

---
+++ leetcode.com/python/140_Word_Break_II.py
@@ -0,0 +1,71 @@
+# plain and simle recursion without memo
+# 31 / 39 test cases passed.
+# TLE
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: List[str]
+        """
+        wordSet = set(wordDict)
+        allSequece = []
+        self.wordreakHelperDFS(s, wordSet, 0, [], allSequece)
+        return allSequece
+
+    def wordreakHelperDFS(self, s, wordSet, srtIdx, currSqn, allSqn):
+        if srtIdx >= len(s):
+            allSqn.append(" ".join(currSqn))
+            return
+
+        for i in range(srtIdx, len(s)):
+            currentWord = s[srtIdx:i + 1]
+            if currentWord in wordSet:
+                currSqn.append(currentWord)
+                self.wordreakHelperDFS(s, wordSet, i + 1, currSqn, allSqn)
+                currSqn.pop()
+
+
+# plain and simle recursion with memo
+# 27 / 39 test cases passed.
+# Getting wrong answer at 28th test case
+from collections import defaultdict
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: List[str]
+        """
+        wordSet = set(wordDict)
+        allSequece = []
+        memo = {}
+        self.wordreakHelperDFS(s, wordSet, 0, [], allSequece, memo)
+        return allSequece
+
+    def wordreakHelperDFS(self, s, wordSet, srtIdx, currSqn, allSqn, memo):
+        if srtIdx >= len(s):
+            allSqn.append(" ".join(currSqn))
+            return
+
+        if s[srtIdx:] in memo:
+            currSqn.append(memo[s[srtIdx:]])
+            allSqn.append(" ".join(currSqn))
+            return
+
+        for i in range(srtIdx, len(s)):
+            currentWord = s[srtIdx:i + 1]
+            if currentWord in wordSet:
+                currSqnLen = len(currSqn)
+                currSqn.append(currentWord)
+
+                self.wordreakHelperDFS(s, wordSet, i + 1, currSqn, allSqn, memo)
+
+                resultOfTheRest = list(currSqn[currSqnLen:])
+                memo[s[srtIdx:]] = " ".join(resultOfTheRest)
+                for j in range(currSqnLen, len(currSqn)):
+                    currSqn.pop()
+
+
+
+
