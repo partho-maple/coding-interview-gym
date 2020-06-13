f7fc1c8d7816d2b38ea3ab25f1715e901b56b8b0
Partho Biswas <partho.biswas@aurea.com>
2020-06-13 12:43:23 +0600

    no message

--- leetcode.com/python/15_3Sum.py
+++ leetcode.com/python/15_3Sum.py
@@ -54,11 +54,36 @@ class Solution(object):
         return result
 
 
-sol = Solution()
-# input = [82597,-9243,62390,83030,-97960,-26521,-61011,83390,-38677,12333,75987,46091,83794,19355,-71037,-6242,-28801,324,1202,-90885,-2989,-95597,-34333,35528,5680,89093,-90606,50360,-29393,-27012,53313,65213,99818,-82405,-41661,-3333,-51952,72135,-1523,26377,74685,96992,92263,15929,5467,-99555,-43348,-41689,-60383,-3990,32165,65265,-72973,-58372,12741,-48568,-46596,72419,-1859,34153,62937,81310,-61823,-96770,-54944,8845,-91184,24208,-29078,31495,65258,14198,85395,70506,-40908,56740,-12228,-40072,32429,93001,68445,-73927,25731,-91859,-24150,10093,-60271,-81683,-18126,51055,48189,-6468,25057,81194,-58628,74042,66158,-14452,-49851,-43667,11092,39189,-17025,-79173,13606,83172,92647,-59741,19343,-26644,-57607,82908,-20655,1637,80060,98994,39331,-31274,-61523,91225,-72953,13211,-75116,-98421,-41571,-69074,99587,39345,42151,-2460,98236,15690,-52507,-95803,-48935,-46492,-45606,-79254,-99851,52533,73486,39948,-7240,71815,-585,-96252,90990,-93815,93340,-71848,58733,-14859,-83082,-75794,-82082,-24871,-1
-input = [-1, 0, 1, 2, -1, -4]
-triplets = sol.threeSum(input)
-print("Result: ", triplets)
+
+# My solution during Mock
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not nums or len(nums) < 2:
+            return []
+        sortedNums = sorted(nums)
+        results = []
+        resultMap = {}
+        for first in range(0, len(sortedNums) - 2):
+            firstNum = sortedNums[first]
+            second, third = first + 1, len(sortedNums) - 1
+            while second < third:
+                secondNum, thirdNum = sortedNums[second], sortedNums[third]
+                if (firstNum + secondNum + thirdNum) == 0:
+                    key = str(firstNum) + "-" + str(secondNum) + "-" + str(thirdNum)
+                    if key not in resultMap:
+                        results.append([firstNum, secondNum, thirdNum])
+                        resultMap[key] = 1
+                    second += 1
+                    third -= 1
+                elif (firstNum + secondNum + thirdNum) > 0:
+                    third -= 1
+                else:
+                    second += 1
+        return results
 
 
 
--- leetcode.com/python/17_Letter_Combinations_of_a_Phone_Number.py
+++ leetcode.com/python/17_Letter_Combinations_of_a_Phone_Number.py
@@ -31,9 +31,42 @@ class Solution(object):
                 currentCombination = currentCombination[:-1]  # Backtrack, remove last character from currentCombination
 
 
+# My solution during mock
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        digitsMap = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        letters = []
+        for digit in digits:
+            letters.append(digitsMap[digit])
+
+        allCombinations = []
+        self.letterCombinationsHelper(letters, 0, [], allCombinations)
+        return allCombinations
+
+    def letterCombinationsHelper(self, letters, currentIdx, currentComb, allComb):
+        if currentIdx >= len(letters):
+            allComb.append("".join(currentComb))
+            return
+
+        for letter in letters[currentIdx]:
+            currentComb.append(letter)
+            self.letterCombinationsHelper(letters, currentIdx + 1, currentComb, allComb)
+            currentComb.pop()  # backtrack
 
 
-sol = Solution()
-input = "23"
-output = sol.letterCombinations(input)
-print('Res: ', output)
