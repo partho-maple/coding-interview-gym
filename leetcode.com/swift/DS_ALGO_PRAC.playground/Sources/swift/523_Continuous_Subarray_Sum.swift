b0026a894fa2a24bc3a64b97b84eb15cc5ab10b4
Partho Biswas <partho.biswas@aurea.com>
2020-06-28 22:18:53 +0600

    523. Continuous Subarray Sum

--- leetcode.com/swift/DS_ALGO_PRAC.playground/Sources/swift/523_Continuous_Subarray_Sum.swift
+++ leetcode.com/swift/DS_ALGO_PRAC.playground/Sources/swift/523_Continuous_Subarray_Sum.swift
@@ -1,6 +1,5 @@
 /*
 // Initial solution. Time complexity: O(n^2)
-import Foundation
 class Solution {
     func checkSubarraySum(_ nums: [Int], _ k: Int) -> Bool {
         guard nums.count > 1 else {return false}
@@ -26,3 +25,33 @@ class Solution {
     }
 }
  */
+// https://tinyurl.com/yb3ya47s
+class Solution {
+    func checkSubarraySum(_ nums: [Int], _ k: Int) -> Bool {
+        guard nums.count > 1 else {return false}
+        
+        var prefixSum = 0
+        var remainderMap = [-1:0] // <index:remainder>, here remainder = prefixSum % k
+        for (endIdx, item) in nums.enumerated() {
+            prefixSum += item
+            if k != 0 {
+                prefixSum = prefixSum % abs(k)
+            } else {
+                if (prefixSum == 0) && (endIdx) >= 1 {
+                    return true
+                }
+            }
+            
+            if let startIdx = remainderMap[prefixSum] {
+                if (endIdx - startIdx) > 1 {
+                    return true
+                }
+                if (k == 0) && (prefixSum == 0) && (endIdx - startIdx) > 1 {
+                    return true
+                }
+            }
+            remainderMap[prefixSum] = endIdx
+        }
+        return false
+    }
+}
