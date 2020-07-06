4d1a3558a365b12f04690ec3ce1990047735ba10
Partho Biswas <partho.biswas@aurea.com>
2020-07-06 23:58:43 +0600

    670. Maximum Swap

--- leetcode.com/swift/DS_ALGO_PRAC.playground/Sources/swift/670_Maximum_Swap.swift
+++ leetcode.com/swift/DS_ALGO_PRAC.playground/Sources/swift/670_Maximum_Swap.swift
@@ -3,9 +3,7 @@ import Foundation
 class Solution {
     func maximumSwap(_ num: Int) -> Int {
         var digits: [Int] = String(num).compactMap{ $0.wholeNumberValue }
-        print("Digits: \(digits)")
         let sortedDigits: [Int] = digits.sorted {$0 > $1}
-        print("Sorted: \(sortedDigits)")
         var numIdxMap = [Int:Int]()
         for (idx, item) in digits.enumerated() {
             numIdxMap[item] = idx
@@ -14,12 +12,35 @@ class Solution {
         while swapIdx < digits.count {
             if digits[swapIdx] != sortedDigits[swapIdx] {
                 let targetIdx = numIdxMap[sortedDigits[swapIdx]]!
-                print("targetIdx: \(targetIdx), swapIdx: \(swapIdx)")
                 digits.swapAt(targetIdx, swapIdx)
                 break
             }
             swapIdx += 1
         }
         return Int(digits.reduce("") { $0 + String($1) }) ?? num
+    }
+}
+
+
+
+import Foundation
+// Time: O(n)
+class Solution {
+    func maximumSwap(_ num: Int) -> Int {
+        var digits: [Int] = String(num).compactMap{ $0.wholeNumberValue }
+        var numIdxMap = [Int:Int]()
+        for (idx, item) in digits.enumerated() {
+            numIdxMap[item] = idx
+        }
+        
+        for currentIdx in 0..<digits.count {
+            for numToSwapWith in stride(from: 9, to: digits[currentIdx], by: -1) {
+                if let targetIdx = numIdxMap[numToSwapWith], targetIdx > currentIdx {
+                    digits.swapAt(currentIdx, targetIdx)
+                    return Int(digits.reduce("") { $0 + String($1) }) ?? num
+                }
+            }
+        }
+        return num
     }
 }
