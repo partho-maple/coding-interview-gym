0b7a1718bdee9e49da55b8be36f517677df9f28e
Partho Biswas <partho.biswas@aurea.com>
2020-08-05 01:07:46 +0600

    636. Exclusive Time of Functions

--- leetcode.com/swift/DS_ALGO_PRAC.playground/Sources/swift/636_Exclusive_Time_of_Functions.swift
+++ leetcode.com/swift/DS_ALGO_PRAC.playground/Sources/swift/636_Exclusive_Time_of_Functions.swift
@@ -4,19 +4,12 @@ class Solution {
         var funcTimeMap = Array(repeating: 0, count: n)
         var funcStack = [Int]()
         var prevTime = 0
-        
         for log in logs {
             let logItem = log.split(separator: ":")
-            let (functionOptional, type, timeOptional) = (Int(logItem[0]), logItem[1], Int(logItem[2]))
-            guard let function = functionOptional, let time = timeOptional else {continue}
-            
-            // print("-----")
-            // print("func: \(function), type: \(type), time: \(time), prevTime: \(prevTime)")
-            // print(funcTimeMap)
-            // print(funcStack)
-            
+            let (funct, type, tim) = (Int(logItem[0]), logItem[1], Int(logItem[2]))
+            guard let function = funct, let time = tim else {continue}
             if type == "start" {
-                if funcStack = funcStack.count > 0 {
+                if funcStack.count > 0 {
                     funcTimeMap[funcStack.last!] += (time - prevTime)
                 }
                 prevTime = time
