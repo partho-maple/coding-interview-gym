d000b59b8bd3621594f954dce9855887ba89b015
Partho Biswas <partho.biswas@aurea.com>
2020-08-24 22:02:01 +0600

    no message

--- leetcode.com/swift/DS_ALGO_PRAC.playground/Contents.swift
+++ leetcode.com/swift/DS_ALGO_PRAC.playground/Contents.swift
@@ -1,1 +1,16 @@
-import Foundation
+class Solution {
+    func groupStrings(_ strings: [String]) -> [[String]] {
+        var groupMap = [String:[String]]()
+        strings.forEach { string in
+            let patttern = getPatternKey(string)
+            groupMap[patttern, default: [String]()] += [string]
+        }
+        return Array(groupMap.values)
+    }
+    
+    func getPatternKey(_ string: String) -> String {
+        let asciiValues = string.unicodeScalars.map({ Int($0.value) })
+        let diffValue = asciiValues.map({ (26 + $0 - asciiValues[0]) % 26 })
+        return diffValue.reduce("", { $0 + " \($1)" })
+    }
+}
