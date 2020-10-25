5e6bf5adc7e565b1b6ed8d33fa260cb872af5fbd
Partho Biswas <partho.maple@gmail.com>
2020-10-25 23:27:00 +0600

    no message

--- leetcode.com/swift/DS_ALGO_PRAC.playground/Contents.swift
+++ leetcode.com/swift/DS_ALGO_PRAC.playground/Contents.swift
@@ -1,1 +1,22 @@
+import Foundation
 
+class Solution {
+    func slowestKey(_ releaseTimes: [Int], _ keysPressed: String) -> Character {
+        let keys = Array(keysPressed)
+        var times = [Character: Int]()
+        for i in 0..<keys.count {
+            if i == 0 {
+                times[keys[i], default: 0] = releaseTimes[i]
+            } else {
+                times[keys[i], default: 0] = releaseTimes[i] - releaseTimes[i - 1]
+            }
+        }
+        
+        var timeCharMap = [Int: Character]()
+        for (key, value) in times {
+            timeCharMap[value] = key
+        }
+        
+        return timeCharMap[timeCharMap.keys.sorted().first]
+    }
+}
