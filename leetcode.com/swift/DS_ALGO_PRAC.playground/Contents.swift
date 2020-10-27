d6fc8f1b36892eca3505ae589142d32f4b34f724
Partho Biswas <partho.maple@gmail.com>
2020-10-27 23:46:54 +0600

    no message

--- leetcode.com/swift/DS_ALGO_PRAC.playground/Contents.swift
+++ leetcode.com/swift/DS_ALGO_PRAC.playground/Contents.swift
@@ -1,2 +1,22 @@
 import Foundation
-
+class Solution {
+    func minimumEffortPath(_ heights: [[Int]]) -> Int {
+        guard !heights.isEmpty, !heights[0].isEmpty else {
+            return 0
+        }
+        var heights = heights
+        var minEffort = heights[0][0]
+        var queue = Array([0,0])
+        while !queue.isEmpty {
+            var currentCell = queue.removeFirst()
+            var (currentX, currentY) = (currentCell[0], currentCell[1])
+            var currentMinEffort = heights[currentX][currentY]
+            var currentLevelSize = queue.count
+            
+            for _ in 0..<currentLevelSize {
+                
+            }
+            minEffort = max(minEffort, currentMinEffort)
+        }
+    }
+}
